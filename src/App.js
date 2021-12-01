a46e19dbc985d3589eaf79169e100d53f40558c7
Manuel Tejada <manuel.tejada95@outlook.com>
2021-11-30 20:22:32 -0600

    corrigiendo advertencias de react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import  Holamundo from './components/Holamundo';
-import Contador from './components/Contador';
-import Pokemon from './components/Pokemon'
+// import  Holamundo from './components/Holamundo';
+// import Contador from './components/Contador';
+// import Pokemon from './components/Pokemon'
 import Pokemones from './components/Pokemones'
 function App() {
   return (
