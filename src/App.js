5ac2f147a41c9e11b4ae595fe7b5d52f295c10ae
Manuel Tejada <manuel.tejada95@outlook.com>
2021-12-06 20:26:47 -0600

    eliminando imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import  Holamundo from './components/Holamundo';
-import Contador from './components/Contador';
-import Pokemon from './components/Pokemon'
+
 import Pokemones from './components/Pokemones'
 function App() {
   return (
